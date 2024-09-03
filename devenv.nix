d5edd56914abe0e831f00da446b9586c6a99b9da
Dom Rodriguez <shymega@shymega.org.uk>
2024-09-03 16:27:16 +0100

    feat: Add `devenv` support

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  packages = with pkgs; [ openssl.dev ];
+  languages = {
+    nix.enable = true;
+    rust.enable = true;
+    shell.enable = true;
+  };
+  devcontainer.enable = true;
+  difftastic.enable = true;
+  pre-commit.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+    clippy.settings.offline = false;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    actionlint.enable = true;
+    nixpkgs-fmt.enable = true;
+    markdownlint.enable = true;
+    statix.enable = true;
+  };
+}
